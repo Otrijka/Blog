ec2882d9dda1d2b9d1740c86fc84a9d39d623e0b
Otrijka <122968877+Otrijka@users.noreply.github.com>
2023-11-19 23:02:33 +0700

    Главная страница
    Поменял валидатор телефона, теперь он не библиотечный
    Сделал липкий футер
    Сделал часть главной сраницы - фильтры
    Сделал генерацию query после нажатия кнопки 'применить'

--- Scripts/MainPage_logic.js
+++ Scripts/MainPage_logic.js
@@ -14,7 +14,7 @@ class Post {
     tags = []
 }
 
-class PageFilters{
+class PageFilters {
     tags = undefined
     author = undefined
     min = undefined
@@ -24,22 +24,65 @@ class PageFilters{
     page = 1
     size = 5
 
-    BuildQuery(){
+    BuildQuery() {
         let query = '?'
         const queryParts = []
-        for (const key in this){
-            if (this[key] !== undefined){
+
+        for (const key in this) {
+            if (this[key] !== undefined && this[key] !== "") {
                 const value = this[key]
-                queryParts.push(`${key}=${value}`)
+
+                if (Array.isArray(value)) {
+                    value.forEach(item => {
+                        queryParts.push(`${key}=${item}`)
+                    })
+                } else {
+                    queryParts.push(`${key}=${value}`)
+                }
             }
         }
-        query = queryParts.join('$')
+        query += queryParts.join('&')
         return query
     }
 }
 
-
-function getPostList(){
-
-
+function getTags() {
+    fetch('https://blog.kreosoft.space/api/tag', {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json'
+        }
+    })
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            let tagFilter = document.querySelector('#filter-tags')
+            data.forEach(tag => {
+                let newOption = document.createElement('option')
+                newOption.text = tag.name
+                newOption.value = tag.id
+                tagFilter.add(newOption)
+            })
+        })
 }
+
+getTags()
+document.querySelector('#btn-apply').addEventListener('click', () => {
+    filters = new PageFilters()
+    filters.author = document.querySelector('#filter-author-name').value
+    filters.tags = Array.from(document.querySelector('#filter-tags').selectedOptions).map(option => option.value)
+    filters.min = document.querySelector('#filter-min-reading-time').value
+    filters.max = document.querySelector('#filter-max-reading-time').value
+    filters.sorting = document.querySelector('#filter-sorting-by').value
+    filters.onlyMyCommunities = document.querySelector('#filter-own-group').checked
+
+    let newQuery = filters.BuildQuery()
+    window.history.pushState({}, '', window.location.pathname + newQuery)
+})
+
+window.onload = () =>{
+    window.history.pushState({},'',window.location.origin)
+}
+
+
--- Scripts/Route.js
+++ Scripts/Route.js
@@ -1,11 +1,9 @@
-
-
-function route(){
+function route() {
     let contentPlace = document.querySelector('#content-place')
     let url = window.location.pathname
     let pageName = ''
 
-    switch (url){
+    switch (url) {
         case '/registration':
             pageName = 'RegistrationPage'
             break;
@@ -20,16 +18,16 @@ function route(){
             break
     }
 
-    fetch('../Pages/' + 'MainPage' + '.html')
-        .then((response) =>{
+    fetch('../Pages/' + pageName + '.html')
+        .then((response) => {
             return response.text();
         })
-        .then((html) =>{
-            contentPlace.innerHTML =  html
-            contentPlace.querySelectorAll('script').forEach(script =>{
+        .then((html) => {
+            contentPlace.innerHTML = html
+            contentPlace.querySelectorAll('script').forEach(script => {
                 const newScript = document.createElement("script")
-                Array.from(script.attributes).forEach(attr =>{
-                    newScript.setAttribute(attr.name,attr.value)
+                Array.from(script.attributes).forEach(attr => {
+                    newScript.setAttribute(attr.name, attr.value)
                 })
                 newScript.appendChild(document.createTextNode(script.innerHTML))
                 script.parentNode.replaceChild(newScript, script)
