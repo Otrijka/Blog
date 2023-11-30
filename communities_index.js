0a8d12987ecdc35173187087235c38db41e3e7fc
Otrijka <122968877+Otrijka@users.noreply.github.com>
2023-11-30 18:50:27 +0700

    Сделал страницу со списком групп
    Ускорил смену вида кнопок 'Подписаться' и 'Отписаться', убрав await для запроса на сервер

---
+++ communities_index.js
@@ -0,0 +1,8 @@
+import {CommunitiesController} from "./Controller/CommunitiesController.js";
+
+let communitiesController = new CommunitiesController()
+
+await communitiesController.init()
+
+
+document.querySelector('#comm')
