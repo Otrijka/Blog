3d22b6f571ee55e0d214db93dd28dba96f9b9a12
Otrijka <122968877+Otrijka@users.noreply.github.com>
2023-12-07 00:01:46 +0700

    вынес функцию создания адреса

--- Controller/CreatePostController.js
+++ Controller/CreatePostController.js
@@ -1,119 +1,7 @@
 import {CreatePostModel} from "../Model/CreatePostModel.js";
 import {CreatePostView} from "../View/CreatePostView.js";
 import {MAIN_PAGE} from "../Constants/dimens.js";
-import {checkToken, getPageHtml, getToken, smoothScrollToBottom} from "../Functions/functions.js";
-
-
-async function newSelectF(parentAddress, currentAddressLevel, addressInfo) {
-
-    let addressHandler = document.querySelector('#create-post-address-handler');
-    let template = await getPageHtml('/Templates/AddressSelectTemplate');
-    let newContainerId = 'create-post-address-level-' + currentAddressLevel;
-    let newSelectId = 'create-post-select-level-' + currentAddressLevel;
-    let newLabelId = 'create-post-label-' + currentAddressLevel;
-
-    let templateContainer = document.createElement('div');
-    templateContainer.id = newContainerId;
-    templateContainer.classList.add('form-group', 'mb-2');
-
-    templateContainer.innerHTML = template;
-
-    if (currentAddressLevel === 1) {
-        templateContainer.querySelector('#create-post-label-').innerText = 'Субъект РФ'
-    }
-
-    templateContainer.querySelector('#create-post-select-level-').id = newSelectId;
-    templateContainer.querySelector('#create-post-label-').id = newLabelId;
-    templateContainer.querySelector('#' + newLabelId).htmlFor = newSelectId;
-    addressHandler.appendChild(templateContainer);
-
-    let newSelect = document.querySelector('#' + newSelectId);
-
-    $(newSelect).select2({
-        ajax: {
-            url: "https://blog.kreosoft.space/api/address/search",
-            dataType: 'json',
-            type: 'GET',
-            data: function (params) {
-                return {
-                    parentObjectId: parentAddress,
-                    query: params.term
-                };
-            },
-            processResults: function (data) {
-                data.unshift({objectId: "", text: "Не выбрано", objectGuid: "", objectLevelText: ""});
-                return {
-                    results: data.map(item => ({
-                        id: item.objectId,
-                        text: item.text,
-                        guid: item.objectGuid,
-                        levelText: item.objectLevelText
-                    }))
-                };
-            }
-        },
-    });
-
-    let placeholderOption = new Option('Не выбрано', {
-        objectId: "",
-        text: "Не выбрано",
-        objectGuid: "",
-        objectLevelText: ""
-    }, true, true);
-    $(newSelect).append(placeholderOption).trigger('change');
-
-    $(newSelect).on('select2:select', async function (e) {
-        let address = {
-            id: e.params.data.id,
-            guid: e.params.data.guid,
-            text: e.params.data.text,
-            levelText: e.params.data.levelText,
-        };
-
-        if (newLabelId === 'create-post-label-1'){
-            document.querySelector('#' + newLabelId).innerText = 'Субъект РФ'
-        }else{
-            document.querySelector('#' + newLabelId).innerText = (address.levelText !== '') ? address.levelText : "Следующий элемент адреса";
-
-        }
-
-        if (addressInfo[currentAddressLevel] !== undefined) {
-            addressInfo[currentAddressLevel] = address;
-            addressInfo.splice(currentAddressLevel + 1)
-        } else {
-            addressInfo.push(address)
-        }
-        console.log("Текущая цепочка");
-        console.log(addressInfo);
-        console.log("---------------");
-
-        const nextData = await (await fetch('https://blog.kreosoft.space/api/address/search?parentObjectId=' + address.id)).json()
-        clearSelects(newContainerId)
-        if (nextData.length === 0) {
-            return
-        }
-
-        if (address.id !== '') {
-            await newSelectF(address.id, currentAddressLevel + 1, addressInfo);
-            smoothScrollToBottom()
-        }
-    });
-}
-
-function clearSelects(selectRef) {
-    let addressHandler = document.querySelector('#create-post-address-handler');
-    let childrenArray = Array.from(addressHandler.children);
-
-    let index = childrenArray.indexOf(document.querySelector('#' + selectRef));
-
-    if (index !== -1) {
-        let newArray = childrenArray.slice(0, index + 1);
-
-        addressHandler.innerHTML = '';
-
-        newArray.forEach(child => addressHandler.appendChild(child));
-    }
-}
+import {addAddressSelect, checkToken, getPageHtml, getToken, smoothScrollToBottom} from "../Functions/functions.js";
 
 class CreatePostController {
     model
@@ -142,7 +30,7 @@ class CreatePostController {
         await this.view.renderCommunities(communities)
         this.view.renderCommunityValue(this.model.getCommunityValueFromQuery(this.query))
 
-        await newSelectF(0, this.currentAddressLevel, this.addressInfo)
+        await addAddressSelect(0, this.currentAddressLevel, this.addressInfo)
     }
 
     async createPost() {
@@ -162,15 +50,14 @@ class CreatePostController {
 
         if (Object.values(validateResult).some(value => value === false)) {
             this.view.showErrors(validateResult)
-            console.log(data)
-            console.log(validateResult)
             return
         }
 
 
-        console.log(await this.model.sendNewPostData(data))
+        await this.model.sendNewPostData(data)
         window.location.href = MAIN_PAGE
     }
 }
+
 
 export {CreatePostController}
